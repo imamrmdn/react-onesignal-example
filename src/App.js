a42f51b66939353c4f46ea734fed4b04895b9cc3
imamrmdn <imamramadhan420@gmail.com>
2021-01-28 01:12:05 +0700

    [fix] config OneSignal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //config script onesignal
+  window.OneSignal = window.OneSignal || [];
+  const OneSignal = Window.OneSignal;
+
+  useEffect(() => {
+
+    OneSignal.push(() => {
+      OneSignal.init(
+        {
+          appId: "752ff749-d5de-48be-b426-b198ee3640b2",
+          promptOptions: {
+            slidedown: {
+              enabled: true,
+              actionMessage: "We'd like to show you notifications for the latest news and updates about the following categories.",
+              acceptButtonText: "Accept!",
+              cancelButtonText: "Cancel",
+            }
+          },
+          welcomeNotification: {
+            "title": "HELLO USERS",
+            "message": "Thanks for subscribing!",
+          }
+        },
+        //Automatically subscribe to the new_app_version tag
+        OneSignal.sendTag("new_app_version", "new_app_version", tagsSent => {
+          // Callback called when tag has finished sending
+          console.log('new_app_version TAG SENT', tagsSent);
+        })
+      );
+    });
+
+  },[OneSignal])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App testing with OneSignal.
         </p>
         <a
           className="App-link"
